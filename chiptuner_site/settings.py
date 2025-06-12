0773836c92319e4a6cd2da624ab5b47e342c6acc
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-11 18:23:43 -0700

    adjust settings again

--- chiptuner_site/settings.py
+++ chiptuner_site/settings.py
@@ -14,19 +14,23 @@ import os
 import sys
 from pathlib import Path
 
+from django.core.exceptions import ImproperlyConfigured
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # get vars from the dotenv
-load_dotenv(BASE_DIR / ".env", override=False)
+# 6/11 - refactor to smoke out problems with automated test build
+load_dotenv(override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY") or os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ImproperlyConfigured("Developer error: SECRET_KEY must be set")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 

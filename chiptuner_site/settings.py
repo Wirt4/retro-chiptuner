b40183142d7cd693ab4cf1563bf70c186de7c9c6
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-11 18:08:11 -0700

    update load_dotenv invocation

--- chiptuner_site/settings.py
+++ chiptuner_site/settings.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # get vars from the dotenv
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / ".env", override=False)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/

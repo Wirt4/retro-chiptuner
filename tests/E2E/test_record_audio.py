d7373bb48d0bedd534d6a21f5712ddd91bfbd32b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-14 19:37:48 -0700

    red

--- tests/E2E/test_record_audio.py
+++ tests/E2E/test_record_audio.py
@@ -3,9 +3,20 @@ from playwright.sync_api import expect
 
 def test_record_audio_flow(custom_page):
     page = custom_page
+    uploads = []
+
+    # set an intercepter to catch the uploads
+    def handle_upload(route, request):
+        uploads.append(request)
+        route.continue_()
+
+    # listen to the "**/upload_audio" route
+    page.route("**/upload_audio", handle_upload)
+
     page.goto("http://localhost:8000")
-    locator = page.get_by_role("button", name="Stop")
-    expect(locator).to_have_count(0)
     page.click("text=Record")
-    page.wait_for_timeout(30)
+    page.wait_for_timeout(3000)
     page.click("text=Stop")
+    page.wait_for_timeout(1000)
+
+    assert len(uploads) > 0, "No upload request was captured"
